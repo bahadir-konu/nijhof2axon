d5b5a6bc486877a4c2a3b536bfafc547e4856324
bah.konu@gmail.com <bah.konu@gmail.com@d02e2dad-0f38-a335-9a99-d963fe79bef8>
2011-10-28 05:55:26 +0000

    Sample data problem

--- trunk/addressbook/app/src/main/java/org/axonframework/sample/app/init/SampleDataGenerator.java
+++ trunk/addressbook/app/src/main/java/org/axonframework/sample/app/init/SampleDataGenerator.java
@@ -1,7 +1,9 @@
 package org.axonframework.sample.app.init;
 
 import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.domain.AggregateIdentifier;
 import org.axonframework.sample.app.api.fohjin.command.CreateClientCommand;
+import org.axonframework.sample.app.api.fohjin.command.OpenNewAccountForClientCommand;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -30,6 +32,11 @@ public class SampleDataGenerator implements ApplicationListener {
         if (initialized.compareAndSet(false, true)) {
             commandBus.dispatch(new CreateClientCommand("Sample Client 1"));
             commandBus.dispatch(new CreateClientCommand("Sample Client 2"));
+
+            //BKONU: How to create this kind of sample data?
+            AggregateIdentifier clientId = null;
+            OpenNewAccountForClientCommand accountForClientCommand = new OpenNewAccountForClientCommand
+                    (clientId, "account name");
         }
     }
 }
