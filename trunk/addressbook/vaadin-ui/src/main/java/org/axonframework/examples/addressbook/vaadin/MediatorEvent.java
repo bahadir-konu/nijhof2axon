141f4d2f8ef1c9bdf51271aaeb1167310582c782
bah.konu@gmail.com <bah.konu@gmail.com@d02e2dad-0f38-a335-9a99-d963fe79bef8>
2011-11-27 15:59:32 +0000

    delete old app

--- trunk/addressbook/vaadin-ui/src/main/java/org/axonframework/examples/addressbook/vaadin/MediatorEvent.java
+++
@@ -1,9 +0,0 @@
-package org.axonframework.examples.addressbook.vaadin;
-
-/**
- * User: Bahadir Konu (bah.konu@gmail.com)
- * Date: 2011-11-23
- * Time: 10:07:37 AM
- */
-public interface MediatorEvent {
-}
